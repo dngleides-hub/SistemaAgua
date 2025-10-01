0ec8578eb4f2de2f1f368863621a0028314e4cee
Gleides <dngleides@gmail.com>
2025-10-01 15:23:40 +0200

    algo novo
    vcvc

---
+++ demos (1)/demo/src/main/java/com/Sistema/de/Gestao/SistemaDeGestaDeAcessoAoFuroApplication.java
@@ -0,0 +1,13 @@
+package com.Sistema.de.Gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestaDeAcessoAoFuroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestaDeAcessoAoFuroApplication.class, args);
+	}
+
+}
